bc19916697505b19c2c4ec27aa0f824bd71ae4ee
Valeria <lerik13@gmail.com>
2022-08-31 14:34:40 -0600

    backend: fetch & serve data from express

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products'
 
 const ProductScreen = () => {
 	const { id } = useParams()
-	//console.log(id);
-	const product = products.find((p) => p._id === id)
+	
+	const [product, setProduct] = useState({});
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${id}`)
+			
+			setProduct(data)
+		}
+
+		fetchProduct()
+	}, [id]);
 
 	return <>
 		<Link className='btn btn-light my-3' to='/'>
