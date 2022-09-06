77a263eef3d57e90c9433274d5a93aa5d80a015e
Valeria <lerik13@gmail.com>
2022-09-06 10:43:52 -0600

    migrate from Redux to Redux-toolkit: use configureStore

--- client/src/store.js
+++ client/src/store.js
@@ -1,26 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 
-const reducer = combineReducers({ 
-	productList: productListReducer,
-	productDetails: productDetailsReducer,
-	cart: cartReducer
-})
+// get cart Items from Local storage
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+	? JSON.parse(localStorage.getItem('cartItems'))
+	: []
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
-const initialState = {
+const preloadedState = {
 	cart: {cartItems: cartItemsFromStorage},
 }
 
-const middleware = [thunk]
-
-const store = createStore(
-	reducer, 
-	initialState, 
-	composeWithDevTools(applyMiddleware(...middleware))
-)
+const store = configureStore({
+	reducer: {
+		productList: productListReducer,
+		productDetails: productDetailsReducer,
+		cart: cartReducer
+	},
+	preloadedState,
+	devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
+})
 
 export default store
