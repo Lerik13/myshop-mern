e28544e66a385498d9567e8c331855dc76c67f88
Valeria <lerik13@gmail.com>
2022-09-09 12:47:03 -0600

    client: orderDetails reducer & action

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,7 @@ import {
 	userDetailsReducer,
 	userUpdateProfileReducer
 } from './reducers/userReducers';
-import { orderCreateReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
 
 // get cart Items from Local storage
 const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -40,6 +40,7 @@ const store = configureStore({
 		userDetails: userDetailsReducer,
 		userUpdateProfile: userUpdateProfileReducer,
 		orderCreate: orderCreateReducer,
+		orderDetails: orderDetailsReducer,
 	},
 	preloadedState,
 	devTools: process.env.NODE_ENV !== 'production', //only show devTools when in production
