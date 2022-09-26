f8b03ce88624557a59b53cbddf0a53eebf379b95
Valeria <lerik13@gmail.com>
2022-09-26 13:38:54 -0600

    Product Search: added SearchBox component into Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../actions/userActions'
+import SearchBox from './SearchBox';
 
 const Header = () => {
 	const userLogin = useSelector(state => state.userLogin)
@@ -23,6 +24,8 @@ const Header = () => {
 					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
+						<SearchBox />
+						
 						<Nav className="ms-auto">
 							<LinkContainer to='/cart'>
 								<Nav.Link>
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
@@ -8,13 +9,15 @@ import Message from '../components/Message';
 
 const HomeScreen = () => {
 	const dispatch = useDispatch()
+	const params = useParams();
+	const keyword = params.keyword;
 
 	const productList = useSelector(state => state.productList)
 	const { loading, error, products} = productList
 
 	useEffect(() => {
-		dispatch(listProducts())
-	}, [dispatch]);
+		dispatch(listProducts(keyword))
+	}, [dispatch, keyword]);
 
 	return (
 		<>
